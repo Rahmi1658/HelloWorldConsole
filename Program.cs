2391943e5f60dea1cb4a049b73e95c527708a165
Rahmi tas <159519@student.horizoncollege.nl>
2020-09-24 16:17:06 +0200

    console update

--- Program.cs
+++ Program.cs
@@ -10,6 +10,30 @@ namespace HelloWorldConsole
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Class");
+            Console.Write("Wat is uw naam? ");
+            var ingevuld = Console.ReadLine();
+            Console.WriteLine($"Hallo {ingevuld}");
+            Console.ReadLine();
+
+
+            if (ingevuld == "Rahmi")
+            {
+                Console.WriteLine("Hallo Rahmi");
+            }
+            else
+            {
+                Console.WriteLine("Goedenmorgen");
+            }
+            Console.Write("Wat is uw favorieten videogame");
+
+            var videogame = Console.ReadLine();
+
+            Console.WriteLine($"U heet {ingevuld} en uw favorieten game is {videogame}");
+
+            Console.ReadLine();
         }
     }
 }
